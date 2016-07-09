604dc7f66ff6f8223f7f743060f5ba8fdd79bd57
Thomas Vu <thomas.vu.it@gmail.com>
2016-07-09 11:56:22 -0500

    Update MovieArrayAdapter.java
    Updating for recyclerview.  Currently stuck on where to initialize xml data.

--- app/src/main/java/app/com/example/android/project1/MovieArrayAdapter.java
+++ app/src/main/java/app/com/example/android/project1/MovieArrayAdapter.java
@@ -1,22 +1,68 @@
 package app.com.example.android.project1;
 
-import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-
-import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
 /**
  * Created by typ_ex on 5/21/2016.
  */
-public class MovieArrayAdapter extends ArrayAdapter<MovieResults.Movie>
+public class MovieArrayAdapter extends RecyclerView.Adapter<MovieArrayAdapter.ViewHolder>
 {
     private ArrayList<MovieResults.Movie> movies;
-    private Context context;
+
+    private final int DETAILS = 0;
+    private final int REVIEWS = 1;
+
+    public MovieArrayAdapter(ArrayList<MovieResults.Movie> movies)
+    {
+        this.movies = movies;
+    }
+
+    @Override
+    public int getItemCount()
+    {
+        return this.movies.size();
+    }
+
+    @Override
+    public int getItemViewType(int position)
+    {
+        if (movies.get(position) instanceof DETAILS)
+        {
+            return DETAILS;
+        }
+        else if (movies.get(position) instanceof REVIEWS)
+        {
+            return REVIEWS;
+        }
+        return -1;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType)
+    {
+        RecyclerView.ViewHolder viewHolder;
+        LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
+
+        switch (viewType)
+        {
+            case DETAILS:
+                View v1 = inflater.inflate(R.layout.detail_viewholder, viewGroup, false);
+                viewHolder = new ViewHolder1(v1);
+                break;
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int position)
+    {
+
+    }
+    /*private Context context;
     public MovieArrayAdapter(Context context, ArrayList<MovieResults.Movie> movies)
     {
         super(context, 0, movies);
@@ -47,5 +93,5 @@ public class MovieArrayAdapter extends ArrayAdapter<MovieResults.Movie>
 
         imageView.setAdjustViewBounds(true); //suggested from https://goo.gl/bgw40p
         return imageView;
-    }
+    }*/
 }
